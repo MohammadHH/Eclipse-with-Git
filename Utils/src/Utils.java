ae5dadf10a952e729d81d08155f39046e52003b2
RRps <monkey.d.luffy19991996@gmail.com>
2019-01-27 20:44:40 +0200

    Add a Util class
    Added a Util class with an int Max(int,int) method

---
+++ Utils/src/Utils.java
@@ -0,0 +1,16 @@
+
+public class Utils {
+
+	public static int max(int a,int b) {
+		if(a>b)
+			return a;
+		else 
+			return b;
+		
+	}
+	
+	public static void main(String args[]) {
+		int a=3,b=9;
+		System.out.println(""+max(a,b));
+	}
+}
